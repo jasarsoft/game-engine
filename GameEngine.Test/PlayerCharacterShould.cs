72c01816091c9b7a19f5d315b8c7baa66a7e222d
Edin Jašarević <edinjasar14@gmail.com>
2020-01-02 17:00:49 +0100

    create first test

---
+++ GameEngine.Test/PlayerCharacterShould.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace GameEngine.Test
+{
+    public class PlayerCharacterShould
+    {
+        [Fact]
+        public void BeInexperiencedWhenNew()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+        }
+    }
+}
