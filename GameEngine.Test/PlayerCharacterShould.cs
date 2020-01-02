fdc431a8250555c7aa026283debab567e859c795
Edin Jašarević <edinjasar14@gmail.com>
2020-01-02 18:45:29 +0100

    asserting on numeric values

--- GameEngine.Test/PlayerCharacterShould.cs
+++ GameEngine.Test/PlayerCharacterShould.cs
@@ -78,5 +78,32 @@ namespace GameEngine.Test
 
             Assert.Matches("[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+", sut.FullName);
         }
+
+        [Fact]
+        public void StartWithDefaultHealth()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.Equal(100, sut.Health);
+        }
+
+        [Fact]
+        public void StartWithDefaultHealth_NotEqualExample()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            Assert.NotEqual(0, sut.Health);
+        }
+
+        [Fact]
+        public void IncreaseHealthAfterSleeping()
+        {
+            PlayerCharacter sut = new PlayerCharacter();
+
+            sut.Sleep();
+
+            //Assert.True(sut.Health >= 101 && sut.Health <= 200);
+            Assert.InRange<int>(sut.Health, 101, 200);
+        }
     }
 }
