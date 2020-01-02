49eaf37d1547632f8ab2ba39d270bf7742b9986b
Edin Jašarević <edinjasar14@gmail.com>
2020-01-02 19:02:37 +0100

    asserting on floating point value

---
+++ GameEngine.Test/BossEnemyShould.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace GameEngine.Test
+{
+    public class BossEnemyShould
+    {
+        [Fact]
+        public void HaveCorrectPower()
+        {
+            BossEnemy sut = new BossEnemy();
+
+            Assert.Equal(166.667, sut.TotalSpecialAttackPower, 3);
+        }
+    }
+}
