b16203bee0d18355dfdbc445102110b5aa1b07de
Edin Jašarević <edinjasar14@gmail.com>
2020-01-04 16:15:14 +0100

    refactoring to Data-Driven Test

--- GameEngine.Test/BossEnemyShould.cs
+++ GameEngine.Test/BossEnemyShould.cs
@@ -21,7 +21,7 @@ namespace GameEngine.Test
 
             BossEnemy sut = new BossEnemy();
 
-            Assert.Equal(166.667, sut.TotalSpecialPower, 3);
+            Assert.Equal(166.667, sut.SpecialAttackPower, 3);
         }
     }
 }
